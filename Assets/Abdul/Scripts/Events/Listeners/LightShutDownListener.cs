fba6e9bf3018bfaefb8fcdc5172a795a1b812d8d
roodey145 <64523090+roodey145@users.noreply.github.com>
2023-11-12 19:51:34 +0100

    The material emission shuts down
    The emission of the material will also get affected accordingly with the light

--- Assets/Abdul/Scripts/Events/Listeners/LightShutDownListener.cs
+++ Assets/Abdul/Scripts/Events/Listeners/LightShutDownListener.cs
@@ -9,18 +9,24 @@ public class LightShutDownListener : MonoBehaviour
     [SerializeField] private string _eventName = "ShutLightDown";
     [SerializeField] private bool _shutDown = false;
     [SerializeField] private bool _randomize = true;
-    [Range(0f, 1f)]
-    [SerializeField] private float _randomizePercentage = 0.1f;
+    [SerializeField, Range(0f, 1f)] private float _randomizePercentage = 0.1f;
     [SerializeField] private float _durationInSeconds = 3f; // Shuts comlpetely down after 3 second
     private float _durationCountDown;
     [SerializeField] private float _blinkingSpeedPerSecond = 5; // Blinks 5 time per second
     private float _blinkingTimer = 0;
+
+    [SerializeField] private string _emissionColorName = "_EmissionColor";
+    private MeshRenderer _renderer;
+    private Material _material;
+
     // Start is called before the first frame update
     void Start()
     {
         _light = GetComponent<Light>();
+        _renderer = transform.parent.GetComponent<MeshRenderer>();
+        _material = _renderer.material;
 
-        if(_randomize)
+        if (_randomize)
         {
             _durationInSeconds += Random.Range(0, _durationInSeconds * _randomizePercentage);
             _blinkingSpeedPerSecond += Random.Range(0, _blinkingSpeedPerSecond * _randomizePercentage);
@@ -30,7 +36,7 @@ public class LightShutDownListener : MonoBehaviour
 
         EventListener eventListener = new EventListener((string eventName) => eventName == _eventName, ShutTheLightDown);
         EventsController.RegisterListener(eventListener);
-        //EventsController.RegisterEvent(_eventName); // TODO: Delete Later
+        EventsController.RegisterEvent(_eventName); // TODO: Delete Later
     }
 
     // Update is called once per frame
@@ -44,6 +50,8 @@ public class LightShutDownListener : MonoBehaviour
             {
                 float color = Random.Range(0, maxLightIntensity);
                 _light.color = new Color(color, color, color);
+                _material.SetColor(_emissionColorName, _light.color);
+                _renderer.material = _material;
             }
 
             _durationCountDown -= Time.deltaTime;
